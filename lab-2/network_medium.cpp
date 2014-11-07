5168094352b95d8004bafb1ca81fbc93715a990d
Stuart Alldritt <odyessy.2001@gmail.com>
2014-11-06 22:27:49 -0500

    Basic implementation of the Network_medium.

--- lab-2/network_medium.cpp
+++ lab-2/network_medium.cpp
@@ -2,9 +2,10 @@
 
 using namespace std;
 
-Network_medium::Network_medium(unsigned int ticks_per_second, unsigned int num_comp) : 
+Network_medium::Network_medium(unsigned int ticks_per_second, unsigned int num_comp, float prop_speed) : 
     Simulatable(ticks_per_second),
-    num_computers(num_comp)
+    num_computers(num_comp),
+    propagation_speed(prop_speed)
 {
     
 }
@@ -14,32 +15,84 @@ void Network_medium::run_tick(unsigned long long int tick)
     //Transmit to any computers that need to be notified
     current_tick = tick;
     
+    float cable_length = num_computers * 10.0;
+    
     for( vector<Collision_event>::iterator it = collisions.begin();
-	     it != collisions.end(); 
-	     //Empty )
+	     it != collisions.end(); )
 	{
-		if((*it).off_propagation_length <= num_computers)
+        // If the "signal off" wave has propagated the entire length of the cable...
+		if(it->off_propagation_length > cable_length)
         {
-            collisions.erase(it);
+            // Delete the collision event as we won't ever use this again
+            it = collisions.erase(it);
+            continue;
         }
-        else
+
+        if(it->state == ANTICIPATED)
         {
-            ++it;
+            it->state = TERMINATED;
+        } 
+        else if(it->state == TRANSMITTED)
+        {
+            it->state = ANTICIPATED;
         }
+        
+        if(it->state == TERMINATED)
+        {
+            it->off_propagation_length += propagation_speed * (1 / ticks_per_second);
+        }
+        
+        it->on_propagation_length += propagation_speed * (1 / ticks_per_second);
+        
+        ++it;
 	}
 }
 
 // Call this function every tick while you are transmitting a packet
 void Network_medium::transmit(Computer* sender)
 {
-    //Add this computer to the list of transmitting computers (this is on a per-tick basis)   
-    transmitters.push_back(sender);
+    //Search for this computer in the list of Collision_events
+    for( vector<Collision_event>::iterator it = collisions.begin();
+	     it != collisions.end(); 
+	     ++it )
+	{
+        // If this computer is in the progress of transmitting a packet...
+		if(it->source == sender && !it->state == ANTICIPATED)
+        {
+            // Update the state to show we're still transmitting (this prevents the "signal off" wave from propagating)
+            it->state = TRANSMITTED;
+            return;
+        } 
+	}
     
+    // If we got to here, either this computer hasn't transmitted before or it had, but the signal had gone stale for at least one tick.
+    // We will create a new event to track this transmission
+    Collision_event newEvent;
+    newEvent.state = TRANSMITTED;
+    newEvent.source = sender;
+    newEvent.on_propagation_length = 0.0;
+    newEvent.off_propagation_length = 0.0;
+    
+    collisions.push_back(newEvent);
 }
 
 // Returns true is the network medium AT THE LOCATION OF THE CURRENT COMPUTER is busy
 // Will not return true if YOU are the one transmitting: only other people
 bool Network_medium::is_busy(Computer* sender)
 {
+    // Search to find out if any current transmissions (or ended transmissions) can be detected at the target computer
+    for( vector<Collision_event>::iterator it = collisions.begin();
+	     it != collisions.end(); 
+	     ++it )
+	{
+        // Get the distance between this "transmitting computer" and the computer that is checking the line for busy
+        float distance = abs(sender->get_Id() - it->source->get_Id()) * 10.0;
+        
+		if(it->on_propagation_length >= distance && it->off_propagation_length < distance)
+        {
+            return true;
+        } 
+	}
     
+    return false;
 }
--- lab-2/network_medium.hpp
+++ lab-2/network_medium.hpp
@@ -2,27 +2,35 @@
 #define _NETWORK_MEDIUM_HPP
 
 #include <iostream>
+#include <vector>
 #include "packet.hpp"
 #include "computer.hpp"
+#include "debug_log.hpp"
+
+enum SignalState {
+  TRANSMITTED,
+  ANTICIPATED,
+  TERMINATED
+};
 
 typedef struct Collision_event_s {
 	Computer * source; // The computer that is transmitting the signal
-	unsigned int on_propagation_length; // The length (in nodes) that the propagation of a "signal on" event has propagated
-    unsigned int off_propagation_length; // The length (in nodes) that the propagation of a "signal off" event has propagated
-    unsigned long long int next_on_propagation_tick; // The next tick that the "signal on" propagation will increase
-    unsigned long long int next_off_propagation_tick; // The next tick that the "signal off" propagation will increase
+	float on_propagation_length; // The length (in meters) that the propagation of a "signal on" event has propagated
+    float off_propagation_length; // The length (in meters) that the propagation of a "signal off" event has propagated
+    SignalState state; // If this signal has stopped transmitting and now we're just waiting for the "signal off" to propagate
 } Collision_event;
 
 class Network_medium : public Simulatable {
     public:
-        Network_medium(unsigned int ticks_per_sec, unsigned int num_computer);
+        Network_medium(unsigned int ticks_per_second, unsigned int num_comp, float prop_speed);
         void run_tick(unsigned long long int tick);
         void transmit(Computer* sender);
         bool is_busy(Computer* sender);
 
     private:
         unsigned long long int current_tick;
-        unsigned int num_computers;
+        unsigned int num_computers; 
+        float propagation_speed; // In meters / sec
         
         std::vector<Collision_event> collisions;        
 };
