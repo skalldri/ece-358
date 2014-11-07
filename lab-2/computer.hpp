67550c1be5bbfb8439fe2b72c6ae632ebb9cdd5a
Kevin Nause <knause@eceLinux3.uwaterloo.ca>
2014-11-06 22:21:50 -0500

    Initial commit.

--- lab-2/computer.hpp
+++ lab-2/computer.hpp
@@ -2,13 +2,16 @@
 #define _COMPUTER_HPP
 
 #include "simulatable.hpp"
+#include "network_medium.hpp"
 
 class Computer : public Simulatable {
-    Computer(unsigned int ticks_per_sec);
+    Computer(Network_medium* medium, int id, unsigned int ticks_per_sec);
     
     void run_tick(unsigned long long int tick);
     
-    void collision_callback();
+    private:
+	int id;
+	Network_medium* medium;
 };
 
 #endif
